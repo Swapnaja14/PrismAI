deb7309edd06b0ae8fbd75d15a7b2097bb208c7a
Swapnaja14 <swapnajayadav2021@gmail.com>
2026-02-26 15:51:40 +0530

    Create and design Chat Window

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar.jsx';
 import ChatWindow from './ChatWindow.jsx';
 import {MyContext} from './MyContext.jsx';
 import './App.css';
+import {v1 as uuidv1} from "uuid";
 
 const App = () => {
-  const providerValues = {};
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply]= useState(null);
+  const [currThreadId, setCurrThreadId] = useState(uuidv1());
+
+  const providerValues = {
+      prompt, setPrompt,
+      reply, setReply,
+      currThreadId, setCurrThreadId
+  };
 
   return (
     <div className='app'>
